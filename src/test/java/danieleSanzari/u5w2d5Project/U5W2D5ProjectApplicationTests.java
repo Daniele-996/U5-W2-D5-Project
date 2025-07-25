11301737a2821309d624e63ebc0093596a3d134f
Daniele-996 <danielesanza@gmail.com>
2025-07-25 10:16:06 +0200

    Initial commit

---
+++ src/test/java/danieleSanzari/u5w2d5Project/U5W2D5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package danieleSanzari.u5w2d5Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
